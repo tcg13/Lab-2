caa55b8af92969036daba846201869dbfe06abe5
Taylor Gilchrist <tcg13@pitt.edu>
2016-01-26 01:05:02 -0500

    calculated number going to St. Ives in loop

--- Lab2.java
+++ Lab2.java
@@ -38,7 +38,7 @@ public class Lab2 {
 	    // all of the wives, sacks, cats, and kittens are as well, along
 	    // with the narrator.
 	    // Calculate the number going to St. Ives
-	    total = -1;
+	    total = 2 + numWives + (numWives * numSacksPerWife) + (numWives * numSacksPerWife * numCatsPerSack) + (numWives * numSacksPerWife * numCatsPerSack * numKitsPerCat);
 	} else {
 	    // Otherwise, only the narrator is going!
 	    total = 1;
